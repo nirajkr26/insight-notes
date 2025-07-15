523b6bb2b902aafb90036cf758df391c619427f1
nirajkr26 <nirajkumargupta2642006@gmail.com>
2025-07-15 20:44:06 +0530

    form handling

--- insight-notes-react/src/pages/signup.jsx
+++ insight-notes-react/src/pages/signup.jsx
@@ -1,6 +1,27 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
+import { useState } from 'react';
 
 function SignUp() {
+
+    const [formData, setFormData] = useState({
+        "name": "",
+        "email": "",
+        "password": ""
+    })
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prev) => ({
+            ...prev,
+            [name]: value
+        }))
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(formData);
+    }
+
     return (
         <>
             <div className="flex flex-col items-center justify-center">
@@ -18,26 +39,26 @@ function SignUp() {
                         <h3 className="font-semibold tracking-tight text-2xl text-center">Create Account</h3>
                         <p className="text-sm text-center text-gray-300">Sign up to start organizing your thoughts with AI</p>
                     </div>
-                    <form>
+                    <form onSubmit={handleSubmit}>
                         <div className="p-6 pt-0 space-y-4 flex flex-col">
                             <div className="space-y-2 flex flex-col">
                                 <label htmlFor="name" className="font-semibold tracking-tight px-3">Name</label>
-                                <input type="text" name="name" id="name" placeholder="Enter your name" className="px-3 py-2 rounded-lg focus-visible:border-3 text-sm h-12 bg-gray-900 focus-visible:outline-none" required />
+                                <input value={formData.name} onChange={handleChange} type="text" name="name" id="name" placeholder="Enter your name" className="px-3 py-2 rounded-lg focus-visible:border-3 text-sm h-12 bg-gray-900 focus-visible:outline-none" required />
                             </div>
                             <div className="space-y-2 flex flex-col">
                                 <label htmlFor="email" className="font-semibold tracking-tight px-3">Email</label>
-                                <input type="text" name="email" id="email" placeholder="Enter your email" className="px-3 py-2 rounded-lg focus-visible:border-3 text-sm h-12 bg-gray-900 focus-visible:outline-none" required />
+                                <input value={formData.email} onChange={handleChange} type="text" name="email" id="email" placeholder="Enter your email" className="px-3 py-2 rounded-lg focus-visible:border-3 text-sm h-12 bg-gray-900 focus-visible:outline-none" required />
                             </div>
                             <div className="space-y-2 flex flex-col">
                                 <label htmlFor="password" className="font-semibold tracking-tight px-3">Password</label>
-                                <input type="password" name="password" id="password" placeholder="Enter your password" className="px-3 py-2 rounded-lg focus-visible:border-3 text-sm h-12 bg-gray-900 focus-visible:outline-none" required />
+                                <input value={formData.password} onChange={handleChange} type="password" name="password" id="password" placeholder="Enter your password" className="px-3 py-2 rounded-lg focus-visible:border-3 text-sm h-12 bg-gray-900 focus-visible:outline-none" required />
                             </div>
                         </div>
                         <div className="items-center p-6 pt-0 flex flex-col space-y-4">
-                            <button className="flex justify-center items-center bg-white text-black w-full py-2 rounded-lg font-bold tracking-tight cursor-pointer hover:bg-gray-200">Create Account</button>
+                            <button className="flex justify-center items-center bg-white text-black w-full py-2 rounded-lg font-bold tracking-tight cursor-pointer hover:bg-gray-200" type='submit'>Create Account</button>
                             <SignedOut>
                                 <SignInButton forceRedirectUrl={"/"} className="flex justify-center items-center gap-1 bg-white text-black w-full py-2 rounded-lg font-bold tracking-tight cursor-pointer hover:bg-gray-200" >
-                                    <button >Sign In Using Google <p> <svg width="24" height="24" viewBox="-3 0 262 262" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid"><path d="M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622 38.755 30.023 2.685.268c24.659-22.774 38.875-56.282 38.875-96.027" fill="#4285F4"/><path d="M130.55 261.1c35.248 0 64.839-11.605 86.453-31.622l-41.196-31.913c-11.024 7.688-25.82 13.055-45.257 13.055-34.523 0-63.824-22.773-74.269-54.25l-1.531.13-40.298 31.187-.527 1.465C35.393 231.798 79.49 261.1 130.55 261.1" fill="#34A853"/><path d="M56.281 156.37c-2.756-8.123-4.351-16.827-4.351-25.82 0-8.994 1.595-17.697 4.206-25.82l-.073-1.73L15.26 71.312l-1.335.635C5.077 89.644 0 109.517 0 130.55s5.077 40.905 13.925 58.602l42.356-32.782" fill="#FBBC05"/><path d="M130.55 50.479c24.514 0 41.05 10.589 50.479 19.438l36.844-35.974C195.245 12.91 165.798 0 130.55 0 79.49 0 35.393 29.301 13.925 71.947l42.211 32.783c10.59-31.477 39.891-54.251 74.414-54.251" fill="#EB4335"/></svg></p></button>
+                                    <button >Sign In Using Google <p> <svg width="24" height="24" viewBox="-3 0 262 262" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid"><path d="M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622 38.755 30.023 2.685.268c24.659-22.774 38.875-56.282 38.875-96.027" fill="#4285F4" /><path d="M130.55 261.1c35.248 0 64.839-11.605 86.453-31.622l-41.196-31.913c-11.024 7.688-25.82 13.055-45.257 13.055-34.523 0-63.824-22.773-74.269-54.25l-1.531.13-40.298 31.187-.527 1.465C35.393 231.798 79.49 261.1 130.55 261.1" fill="#34A853" /><path d="M56.281 156.37c-2.756-8.123-4.351-16.827-4.351-25.82 0-8.994 1.595-17.697 4.206-25.82l-.073-1.73L15.26 71.312l-1.335.635C5.077 89.644 0 109.517 0 130.55s5.077 40.905 13.925 58.602l42.356-32.782" fill="#FBBC05" /><path d="M130.55 50.479c24.514 0 41.05 10.589 50.479 19.438l36.844-35.974C195.245 12.91 165.798 0 130.55 0 79.49 0 35.393 29.301 13.925 71.947l42.211 32.783c10.59-31.477 39.891-54.251 74.414-54.251" fill="#EB4335" /></svg></p></button>
                                 </SignInButton>
                             </SignedOut>
                             <button className="flex justify-center items-cente text-gray-200 w-full py-2 rounded-lg font-bold tracking-tight cursor-pointer hover:bg-gray-900">Already have an account? Sign In</button>
