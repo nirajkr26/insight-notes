467c206348b4f9f1bbfd5a27833ad9ced2d43b5f
nirajkr26 <nirajkumargupta2642006@gmail.com>
2025-07-27 23:35:10 +0530

    environment variables

--- Backend/db/conn.js
+++ Backend/db/conn.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://nirajkumar232:niraj123@cluster0.y3up3u5.mongodb.net/notes';
+const MONGODB_URI = process.env.MONGODB_URI;
 
-async function conn(){
+async function conn() {
     await mongoose.connect(MONGODB_URI);
     console.log('Connected to MongoDB');
 }
--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 const cors = require('cors');
 const conn = require('./db/conn.js');
 const userRoutes = require('./routes/userRoutes');
 const noteRoutes = require('./routes/noteRoutes');
-const port = 3000;
+const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
